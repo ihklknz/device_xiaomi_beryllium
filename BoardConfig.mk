a93d5d9c5b71c8dfc979ee34dbbd40cc739ddb68
DD3Boh <dade.garberi@gmail.com>
2020-06-06 16:59:43 +0530

    beryllium: init: Override dalvik props according to total ram
    * Imported from https://github.com/LineageOS/android_device_xiaomi_sdm660-common/tree/lineage-17.1/init
    
    Change-Id: I2e8ffd8b1107ad74f336e505a6bd8dee36f6d0b5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,10 @@ TARGET_USES_ION := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_beryllium
+TARGET_RECOVERY_DEVICE_MODULES := libinit_beryllium
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
