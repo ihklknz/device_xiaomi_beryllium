ab22fbd6971c4fe141663273374d7d2e8d1ecc0c
Alone0316 <rutujamandlik06@gmail.com>
2022-09-16 17:58:24 +0000

    beryllium: Remove packages at build time

--- device.mk
+++ device.mk
@@ -391,6 +391,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Remove packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
