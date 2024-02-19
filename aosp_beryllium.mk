ihklknz/device_xiaomi_beryllium-4.19

beryllium: HorizonDroid Go Official

aosp_beryllium.mk
@@ -11,12 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common HorizonDroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# HorizonDroid stuff.
+CUSTOM_BUILD_TYPE := OFFICIAL
+HORIZON_MAINTAINER := hklknzツ
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := awaken_beryllium
+PRODUCT_NAME := beryllium_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1

ihklknz/device_xiaomi_beryllium

aosp_beryllium.mk
@@ -11,12 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common HorizonDroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# HorizonDroid stuff.
+CUSTOM_BUILD_TYPE := OFFICIAL
+HORIZON_MAINTAINER := hklknzツ
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := awaken_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1